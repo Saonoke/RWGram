65ce60f19f3ded001a4f6f1b0906056ad7978a1e
krisna <saonoke21@gmail.com>
2024-06-10 13:17:03 +0700

    fix : font

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -32,7 +32,7 @@
             <div class="flex  flex-1 items-center justify-between sm:items-stretch ">
                 <div class="flex flex-shrink-0 space-x-3 items-center">
                     <img class="h-10 w-auto" src="https://res.cloudinary.com/dtzlizlrs/image/upload/v1717481970/ioxdtp815fvw1w3smkcc.png" alt="Your Company">
-                    <a href="/" class="text-dodger-blue-950 font-body font-bold text-xl "> <span
+                    <a href="/" class="text-dodger-blue-950  font-extrabold text-xl "> <span
                             class="text-dodger-blue-700">RW</span>GRAM</a>
                 </div>
                 <div class="hidden lg:block">
